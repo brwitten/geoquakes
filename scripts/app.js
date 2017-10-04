5b5f86d996e03e4a9ed8df9773d5a5bfd228179f
lperry34 <leviperry34@gmail.com>
2017-10-04 14:46:02 -0700

    Added a button with bigger earthquakes and then back to smaller earthquakes

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,24 @@
 // define globals
 var geoUrl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson";
-var googleMap = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg"
-
-
 
 $(document).ready(function() {
   console.log("Let's get coding!");
-  // CODE IN HERE!
 
+
+
+	request();
+	$("button").on("click", function(e){
+		e.preventDefault();
+		geoUrl = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+		request();
+	});
+});
+
+
+
+
+function request(){
+	console.log(geoUrl);
   $.ajax({
 	method: "GET",
 	url: geoUrl,
@@ -15,14 +26,17 @@ $(document).ready(function() {
 	error: onError
 });
 
+  }
 
 
 function onSuccess(json) {
-	 for (var i = 0; i < json.features.length; i++) {
-	$("#info").append('<p>'+json.features[i].properties.title+'</p>');
-	console.log(json)
+	$('.quake-line').remove();
+	for (var i = 0; i < json.features.length; i++) {
+		$("#info").append('<p class="quake-line">'+json.features[i].properties.title+'</p>');
+	
+	}
 }
-}
+	
 
 function onError(xhr, status, errorThrown) {
 	alert("Sorry, there was a problem!");
@@ -31,13 +45,12 @@ function onError(xhr, status, errorThrown) {
 	console.dir(xhr);
 }
 
-
-
-
-});
+	
 
 
   var map;
+
+
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           zoom: 2,
@@ -45,17 +58,12 @@ function onError(xhr, status, errorThrown) {
           mapTypeId: 'terrain'
         });
 
-        // Create a <script> tag and set the USGS URL as the source.
         var script = document.createElement('script');
-        // This example uses a local copy of the GeoJSON stored at
-        // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
 
         script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
         document.getElementsByTagName('head')[0].appendChild(script);
       }
 
-      // Loop through the results array and place a marker for each
-      // set of coordinates.
       window.eqfeed_callback = function(results) {
         for (var i = 0; i < results.features.length; i++) {
           var coords = results.features[i].geometry.coordinates;
@@ -63,8 +71,9 @@ function onError(xhr, status, errorThrown) {
           var marker = new google.maps.Marker({
             position: latLng,
             map: map
+          
           });
+
+           
         }
       }
-      
-      console.log(coords);
