1f2baa33dda489394aa18469b3dc3858bdc59ade
lperry34 <leviperry34@gmail.com>
2017-10-04 11:50:46 -0700

    added eqfeed solution

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,37 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var geoUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var googleMap = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBHLett8djBo62dDXj0EjCimF8Rd6E8cxg"
+
+
 
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+
+  $.ajax({
+	method: "GET",
+	url: geoUrl,
+	success: onSuccess,
+	error: onError
+});
+
+
+
+function onSuccess(json) {
+	 for (var i = 0; i < json.features.length; i++) {
+	$("#info").append('<p>'+json.features[i].properties.title+'</p>');
+	console.log(json)
+}
+}
+
+function onError(xhr, status, errorThrown) {
+	alert("Sorry, there was a problem!");
+	console.log("Error: " + errorThrown);
+	console.log("Status: " + status);
+	console.dir(xhr);
+}
+
+
+
 
 });
